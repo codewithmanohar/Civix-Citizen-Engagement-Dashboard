eac0d07a04a35e4f376aa622fafb6ca584d7badf
Manohar Kumar <icsixyz@gmail.com>
2025-08-29 11:26:05 +0530

    Authentication and Authorization Routest Created

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 // connection 
 const connectDB = async (req , res) =>{
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        const conn = await mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/Civic");
         console.log(`Mongodb Connected : ${conn.connection.host}`);
     } catch (error) {
         console.log(`Error : ${error.message}`);
