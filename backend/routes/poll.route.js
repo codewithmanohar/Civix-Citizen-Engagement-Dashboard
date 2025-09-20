4e14b394516edc7a87e8d9d619faa34f22f760d6
Manohar Kumar <icsixyz@gmail.com>
2025-09-20 12:20:58 +0530

    Delete Polls Route Implemented

--- backend/routes/poll.route.js
+++ backend/routes/poll.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createPoll, getPolls, getPollById, getPollResults } from "../controllers/polls.controller.js";
+import { createPoll, getPolls, getPollById, getPollResults, deletePoll } from "../controllers/polls.controller.js";
 import { jwtAuthMiddleware } from "../middlewares/auth.js";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post("/create", jwtAuthMiddleware, createPoll);
 router.get("/", getPolls);                        
 router.get("/:id", getPollById);                  
 router.get("/:id/results", jwtAuthMiddleware, getPollResults);
+router.delete("/:id", jwtAuthMiddleware , deletePoll);
 
 export default router;
